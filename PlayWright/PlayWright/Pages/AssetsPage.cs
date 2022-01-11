aeaab453da01930be5fc931f652c528826ef1beb
sanya lappo <sanyalappo16@gmail.com>
2022-01-11 15:28:50 +0300

    Fixed Assets field

--- PlayWright/PlayWright/Pages/AssetsPage.cs
+++ PlayWright/PlayWright/Pages/AssetsPage.cs
@@ -10,22 +10,29 @@ namespace BybitFramework.Pages
 {
     internal class AssetsPage : BasePage
     {
+        public const string TransferButton = "button:has-text(\"Transfer\")";
+        public const string SenderDropdownOpen = ".asset-transfer__account > .asset-transfer__account-wraper";
+        public const string ChooseSender = "//div[text()='Spot Account']";
+        public const string ChooseReceiver = "//div[text()='Derivatives Account']";
+        public const string CoinDropdownOpen = ".by-select-adv-selection-search-input";
+        public const string TransferableField = ".by-input__inner";
+        public const string ConfirmButton = ".by-button";
         public AssetsPage(IPage page) : base(page) { }
 
         public async Task<AssetsPage> TransferButAsync()
         {
-            await Page.ClickAsync("button:has-text(\"Transfer\")");
+            await Page.ClickAsync(TransferButton);
             return this;
         }
         public async Task<AssetsPage> SenderAccountAsync()
         {
-            await Page.Locator(".asset-transfer__account > .asset-transfer__account-wraper").Nth(1).ClickAsync();
+            await Page.Locator(SenderDropdownOpen).Nth(1).ClickAsync();
             return this;
         }
 
         public async Task<AssetsPage> ChooseSenderAccountAsync()
         {
-            await Page.Locator("//div[text()='Spot Account']").ClickAsync();
+            await Page.Locator(ChooseSender).ClickAsync();
             return this;
         }
 
@@ -35,12 +42,12 @@ namespace BybitFramework.Pages
         }
         public async Task<AssetsPage> ChooseReceiverAccountAsync()
         {
-            await Page.Locator("//div[text()='Derivatives Account']").ClickAsync();
+            await Page.Locator(ChooseReceiver).ClickAsync();
             return this;
         }
         public async Task<AssetsPage> OpenChooseCoinTypeAsync()
         {
-            await Page.Locator(".by-select-adv-selection-search-input").ClickAsync();
+            await Page.Locator(CoinDropdownOpen).ClickAsync();
             return this;
         }
 
@@ -50,12 +57,12 @@ namespace BybitFramework.Pages
         }
         public async Task<AssetsPage> ChooseTransferableAmountAsync(double amount)
         {
-            await Page.FillAsync(".by-input__inner", amount.ToString());
+            await Page.FillAsync(TransferableField, amount.ToString());
             return this;
         }
         public async Task<AssetsPage> ConfirmAsync()
         {
-            await Page.ClickAsync(".by-button");
+            await Page.ClickAsync(ConfirmButton);
             return this;
         }
 
