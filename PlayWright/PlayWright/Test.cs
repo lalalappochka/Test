61516b2d7679c2941957593afebb179274b14b01
sanya lappo <sanyalappo16@gmail.com>
2022-01-13 05:44:50 +0300

    add little

--- PlayWright/PlayWright/Test.cs
+++ PlayWright/PlayWright/Test.cs
@@ -41,10 +41,10 @@ namespace BybitFramework
             FirstPage firstPage = new FirstPage(_page);
             AssetsPage assetsPage = await firstPage.MoveToAssestsPageAsync().Result.TransferButAsync();
             AssetTransfer assetTransfer = new AssetTransfer();
-            //assetTransfer.CashBeforeTransferOperation = await assetsPage.GetCash(TransferSideSelection.RECIEVER);
-            //await assetsPage.TransferOperationAsync(amount);
-            //assetTransfer.CashAfterTransferOperation = await assetsPage.GetCash(TransferSideSelection.RECIEVER);
-            //Assert.AreEqual(assetTransfer.CashBeforeTransferOperation, assetTransfer.CashAfterTransferOperation - amount);
+            assetTransfer.CashBeforeTransferOperation = await assetsPage.GetCash(TransferSideSelection.RECIEVER);
+            await assetsPage.TransferOperationAsync(amount);
+            assetTransfer.CashAfterTransferOperation = await assetsPage.GetCash(TransferSideSelection.RECIEVER);
+            Assert.AreEqual(assetTransfer.CashBeforeTransferOperation, assetTransfer.CashAfterTransferOperation - amount);
         }
 
         [Test]
@@ -52,11 +52,11 @@ namespace BybitFramework
         {
             FirstPage firstPage = new FirstPage(_page);
             MarketPage marketPage = await firstPage.MoveToMarketPageAsync();
-            //await marketPage.ChooseSpotAsync();
-            //string findSpot = await marketPage.FindSpotToAddAsync();
-            //await marketPage.ChooseFavouriteOperation();
-            //string addedSpot = await marketPage.CheckMarketsAsync();
-            //Assert.AreEqual(findSpot, addedSpot);
+            await marketPage.ChooseSpotAsync();
+            string findSpot = await marketPage.FindSpotToAddAsync();
+            await marketPage.ChooseFavouriteOperation();
+            string addedSpot = await marketPage.CheckMarketsAsync();
+            Assert.AreEqual(findSpot, addedSpot);
 
         }
 
@@ -66,10 +66,10 @@ namespace BybitFramework
             FirstPage firstPage = new FirstPage(_page);
             SpotPage spotPage = await firstPage.MoveToSpotPageAsync();
             await spotPage.FindDropdownDerivativesAsync();
-            //string futureDerivate = await spotPage.FindNewDerivativeAsync();
-            //await spotPage.ChooseNewDerivativeAsync();
-            //string curretnDerivative = await spotPage.GetCurrentDerivativeAsync();
-            //Assert.AreEqual(futureDerivate,curretnDerivative);
+            string futureDerivate = await spotPage.FindNewDerivativeAsync();
+            await spotPage.ChooseNewDerivativeAsync();
+            string curretnDerivative = await spotPage.GetCurrentDerivativeAsync();
+            Assert.AreEqual(futureDerivate, curretnDerivative);
         }
 
         [Test]
@@ -78,21 +78,21 @@ namespace BybitFramework
         {
             FirstPage firstPage = new FirstPage(_page);
             ExchangePage exchangePage = await firstPage.MoveToExchangePage();
-            //await exchangePage.ConvertOperation(amount);
-            //string checksumm = await exchangePage.CheckSummOfConvertAsync();
-            //string lastconvert = await exchangePage.LastConvertingAsync();
-            //Assert.AreEqual(checksumm, lastconvert);
+            await exchangePage.ConvertOperation(amount);
+            string checksumm = await exchangePage.CheckSummOfConvertAsync();
+            string lastconvert = await exchangePage.LastConvertingAsync();
+            Assert.AreEqual(checksumm, lastconvert);
         }
 
-         [Test]
+        [Test]
          [TestCase(0.2)]
          public async Task BuyBTC(double amount)
         {
             FirstPage firstPage = new FirstPage(_page);
             SpotPage spotPage = await firstPage.MoveToSpotPageAsync();
-            //await spotPage.ByuingBTCOperation(amount);
-            //double quantity = await spotPage.CheckHistoryAmountAsync();
-            //Assert.AreEqual(amount, quantity);
+            await spotPage.ByuingBTCOperation(amount);
+            double quantity = await spotPage.CheckHistoryAmountAsync();
+            Assert.AreEqual(amount, quantity);
         }
 
         [Test]
@@ -101,9 +101,9 @@ namespace BybitFramework
         {
             FirstPage firstPage = new FirstPage(_page);
             SpotPage spotPage = await firstPage.MoveToSpotPageAsync();
-            //await spotPage.SellingBTCOperation(amount);
-            //double quantity = await spotPage.CheckHistoryAmountAsync();
-            //Assert.AreEqual(amount, quantity);
+            await spotPage.SellingBTCOperation(amount);
+            double quantity = await spotPage.CheckHistoryAmountAsync();
+            Assert.AreEqual(amount, quantity);
         }
     }
 }
